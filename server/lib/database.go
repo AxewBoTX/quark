0555ef36a2913d7a47e99b054de731609fa24aa8
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-03-24 19:21:36 +0530

    generate SQL UPDATE query based on User struct input
    this function takes in a User struct as input and gives out an SQL query
    based on the columns that are provided in the User struct input.

--- server/lib/database.go
+++ server/lib/database.go
@@ -3,6 +3,7 @@ package lib
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 
 	"github.com/charmbracelet/log"
 	_ "modernc.org/sqlite"
@@ -30,4 +31,24 @@ func HandleMigrations(DB *sql.DB) {
 	)`, USER_TABLE_NAME)); table_create_err != nil {
 		log.Fatal("Failed To Create usr_base table", "Error", table_create_err)
 	}
+}
+
+// generate SQL UPDATE query based on User struct input
+func GenerateSQLUpdateQuery(user User) string {
+	var updateFields []string
+	if user.Username != "" {
+		updateFields = append(updateFields, fmt.Sprintf(`username='%s'`, user.Username))
+	}
+	if user.PasswordHash != "" {
+		updateFields = append(updateFields, fmt.Sprintf(`passwordHash='%s'`, user.PasswordHash))
+	}
+	if user.UserAuthToken != "" {
+		updateFields = append(updateFields, fmt.Sprintf(`userAuthToken='%s'`, user.UserAuthToken))
+	}
+	if len(updateFields) == 0 {
+		return ""
+	}
+	setClause := strings.Join(updateFields, ", ")
+	query := fmt.Sprintf("UPDATE %s SET %s WHERE ID='%s';", USER_TABLE_NAME, setClause, user.ID)
+	return query
 }
