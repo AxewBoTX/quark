819be2e541c9de27c548c7f7f26631cd87043e2d
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-04-26 15:00:59 +0530

    lockfiles && updating the `RenderTemplate` function

--- client/web/web.go
+++ client/web/web.go
@@ -1,10 +1,12 @@
 package web
 
 import (
+	"encoding/json"
 	"net/http"
 	"strings"
 
 	"github.com/a-h/templ"
+	"github.com/go-resty/resty/v2"
 	"github.com/labstack/echo/v4"
 
 	"quark/client/lib"
@@ -20,49 +22,64 @@ var Pages = map[string]templ.Component{
 }
 
 func RenderTemplate(c echo.Context, status int) error {
+	client := resty.New()
 	URL := strings.TrimSpace(c.Request().URL.String())
-
-	_, session_cookie_get_err := c.Cookie(lib.SESSION_COOKIE_NAME)
-	if session_cookie_get_err != nil { // if session cookie is not present
-		if strings.HasPrefix(URL, "/chat") {
-			// redirecting the the user to /login route if the user is current at any protected route
+	// check if session cookie is present
+	session_cookie, session_cookie_get_err := c.Cookie(lib.SESSION_COOKIE_NAME)
+	if session_cookie_get_err != nil { // Not Present
+		// Check if user is on protected route
+		if strings.HasPrefix(URL, "/chat") { // protected
 			c.Redirect(http.StatusSeeOther, "/login")
-		} else {
-			//rendering web page templ component according to current URL
-			c.Response().Writer.WriteHeader(status)
-			c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
-			if template_render_err := Pages[URL].Render(c.Request().Context(), c.Response().Writer); template_render_err != nil {
-				lib.ErrorWithColor(
-					"ERROR",
-					"0",
-					lib.COLOR_RED,
-					"Failed To Render Response Template",
-					"Error",
-					template_render_err,
-				)
-				return c.String(http.StatusInternalServerError, "Failed To Render Response Template")
+		} else { // not protected
+			RenderTemplTemplate(c, http.StatusOK)
+		}
+	} else { // Present
+		res, user_fetch_err := client.R().Get(lib.SERVER_HOST + lib.SERVER_PORT + "/users/token/" + session_cookie.Value)
+		if user_fetch_err != nil || res.StatusCode() == http.StatusInternalServerError { // Not A Valid Response
+			// Check if user is on protected route
+			if strings.HasPrefix(URL, "/chat") { // protected
+				c.Redirect(http.StatusSeeOther, "/login")
+			} else { // not protected
+				RenderTemplTemplate(c, http.StatusOK)
+			}
+		} else { // Valid Response
+			// check if user is on protected route
+			if strings.HasPrefix(URL, "/chat") {
+				var user lib.User
+				if resp_decode_err := json.Unmarshal(res.Body(), &user); resp_decode_err != nil {
+					lib.ErrorWithColor(
+						"ERROR",
+						"0",
+						lib.COLOR_RED,
+						"Failed To Decode Server Response",
+						"Error",
+						resp_decode_err,
+					)
+				}
+				c.Set("session-val", user)
+				RenderTemplTemplate(c, http.StatusOK)
+			} else { // not protected
+				c.Redirect(http.StatusSeeOther, "/chat")
 			}
 		}
-	} else { // if session cookie is present
-		if URL == "/" || URL == "/login" || URL == "/register" {
-			// redirecting the the user to /chat route if the user is current at any public unprotected route
-			c.Redirect(http.StatusSeeOther, "/chat")
-		} else {
-			//rendering web page templ component according to current URL
-			c.Response().Writer.WriteHeader(status)
-			c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
-			if template_render_err := Pages[URL].Render(c.Request().Context(), c.Response().Writer); template_render_err != nil {
-				lib.ErrorWithColor(
-					"ERROR",
-					"0",
-					lib.COLOR_RED,
-					"Failed To Render Response Template",
-					"Error",
-					template_render_err,
-				)
-				return c.String(http.StatusInternalServerError, "Failed To Render Response Template")
-			}
-		}
+	}
+	return nil
+}
+
+func RenderTemplTemplate(c echo.Context, status int) error {
+	URL := strings.TrimSpace(c.Request().URL.String())
+	c.Response().Writer.WriteHeader(status)
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+	if template_render_err := Pages[URL].Render(c.Request().Context(), c.Response().Writer); template_render_err != nil {
+		lib.ErrorWithColor(
+			"ERROR",
+			"0",
+			lib.COLOR_RED,
+			"Failed To Render Response Template",
+			"Error",
+			template_render_err,
+		)
+		return c.String(http.StatusInternalServerError, "Failed To Render Response Template")
 	}
 	return nil
 }
